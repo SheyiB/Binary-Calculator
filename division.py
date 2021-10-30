90fb853e3baac75b11aa646d7990a04f2f7b7394
Elijah Oluwaseyifunmi Banjo <elijahbanjo@gmail.com>
2021-10-30 10:41:57 +0100

    Division and Subtraction Init

---
+++ division.py
@@ -0,0 +1,3 @@
+def divisionbuild():
+   
+    print('Under Construction')
---
+++ subtraction.py
@@ -0,0 +1,16 @@
+# def sub(num1,num2):
+#     pass
+    
+#print(bin_check02('9011')) 
+def addbuild():
+    # bin01 = input('Enter first binary \n -->')
+    # bin02 = input('Enter second binary \n -->')
+
+    # print('''
+    #          %s
+    #      -   %s
+    #      -----------
+    #          %s
+    #      -----------
+    #    '''%(bin01,bin02,sub(bin01,bin02,0)))
+    print('Under Construction')
